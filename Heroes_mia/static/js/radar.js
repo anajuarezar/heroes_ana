08a15289da58c3dbae64be30da17efdfcb5711a4
anajuarezar <ana.juarezar@gmail.com>
2021-07-22 22:40:58 -0700

    Final

--- Heroes_mia/static/js/radar.js
+++ Heroes_mia/static/js/radar.js
@@ -1,63 +1,69 @@
+
+
+
 function radarPlot() {
+
     
     // // Creates the Dropdown Menu
     d3.json("data/heroes.json").then((data) => {
+
         var heroeID = [];
         console.log(data);
-
+        
         for (var i = 0; i < data.length; i++) {
             var IDarray = data[i].id
             heroeID.push(IDarray)
         }
-        // console.log(heroeID);
+
         var options = heroeID;
+
         var dropMenuHero1 = d3.select("#selDataset");
         options.map(option => {
         dropMenuHero1.append("option").text(option);
         });
+        var ID1 = dropMenuHero1.property("value");
+        var hero1 =(dropMenuHero1.property("value"))-1;
 
         var dropMenuHero2 = d3.select("#selDataset2");
         options.map(option => {
         dropMenuHero2.append("option").text(option);
         });
-
-        d3.selectAll("#selDataset").on("change", radarPlot);
-        d3.selectAll("#selDataset2").on("change2", radarPlot);
-
-        var hero1 =(dropMenuHero1.property("value"))-1;
-        var hero2 =(dropMenuHero2.property("value"))-1;
-
-        var ID1 = dropMenuHero1.property("value");
         var ID2 = dropMenuHero2.property("value");
+        var hero2 =(dropMenuHero2.property("value"))-1;
+        
 
         var dataHero1 = data[hero1];
         var dataHero2 = data[hero2];
 
-        console.log(dataHero1);
-        console.log(dataHero2);
+        console.log("Hero 1"+ dataHero1);
+        console.log("Hero 2"+ dataHero2);
 
         var graph = document.getElementById("radar");
         var stats1 = Object.values(data[hero1].powerstats)
         var stats2 = Object.values(data[hero2].powerstats)
 
         console.log(data[hero1].name);
+
+        if (myChart) {
+            myChart.destroy();
+        }
         
-        var myChart = new Chart(graph, {
+        myChart = new Chart(graph, {
         type: 'radar',
         data: {
             labels: ['Intelligence', 'Strength', 'Speed', 'Durability', 'Power', 'Combat'],
             datasets: [{
                 label: `ID: ${ID1}: Superheroe: ${data[hero1].name}`,
                 data: stats1,
-                backgroundColor: '#e6f7ff',
-                borderColor: '#009999',
-                borderWidth: 3
+                backgroundColor: 'rgba(255, 99, 132, 0.3)',
+                borderColor: 'rgb(255, 99, 132)',
+                borderWidth: 3,
             },
             {
                 label: `ID: ${ID2}: Superheroe: ${data[hero2].name}`,
                 data: stats2,
-                backgroundColor: '#e6e6ff',
-                borderColor: '#9999ff',
+                backgroundColor: 'rgba(54, 162, 235, 0.3)',
+                borderColor: 'rgb(54, 162, 235)',
                 borderWidth: 3
             }
         ]
@@ -67,4 +73,9 @@ function radarPlot() {
 
 }
 
+var myChart;
+
 radarPlot();
+
+d3.selectAll("#selDataset").on("change", radarPlot);
+d3.selectAll("#selDataset2").on("change", radarPlot);
---
+++ Heroes_mia/static/python/app.py
@@ -0,0 +1,55 @@
+import pymongo
+from flask import Flask, jsonify, render_template
+
+
+def call_superHeroe():
+  myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+  mydb = myclient["superheroes_db"]
+  mycol = mydb["api_superheroes"]
+
+  mydoc = mycol.find({})
+
+  jsonHero = []
+  for x in mydoc:
+    del x['_id']
+    jsonHero.append(x["name"])
+
+  return jsonHero
+
+
+# # 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+
+# 3. Define what to do when a user hits the index route
+@app.route("/")
+def home():
+
+  return render_template("index.html")
+
+
+@app.route("/api")
+def api():
+
+  myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+  mydb = myclient["superheroes_db"]
+  mycol = mydb["api_superheroes"]
+
+  mydoc = mycol.find({})
+
+  jsonHero = []
+  for x in mydoc:
+    del x['_id']
+    jsonHero.append(x)
+
+  return jsonify(jsonHero)
+
+  
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
